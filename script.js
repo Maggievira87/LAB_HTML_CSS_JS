34bc24e52a94cc9ce9fb79940c0b18c01d385889
Maggievira87 <margarethjvr87@gmail.com>
2025-11-06 15:18:20 -0500

    Margareth Solucion del taller

---
+++ script.js
@@ -0,0 +1,22 @@
+let contador = 0;
+const contadorElemento = document.getElementById("contador");
+const btnIncrementar = document.getElementById("incrementar");
+const btnDecrementar = document.getElementById("decrementar");
+function actualizarContador() {
+    contadorElemento.textContent = contador;
+    if (contador >= 10) {
+        contadorElemento.style.color = "Blue";
+    } else {
+        contadorElemento.style.color = "#333";
+    }
+}
+
+btnIncrementar.addEventListener("click", () => {
+    contador++;
+    actualizarContador();
+});
+
+btnDecrementar.addEventListener("click", () => {
+    contador--;
+    actualizarContador();
+});
